30ac4e40a2745513e44d841d5b27d2323f811c03
choc <notchoc@disroot.org>
2024-09-17 11:34:17 +0800

    fix item dupe for eggbug, vulture, mother spider
    hacky, should use IL hooks to disable clientside spawns

--- Game/RainMeadow.CreatureHooks.cs
+++ Game/RainMeadow.CreatureHooks.cs
@@ -18,6 +18,10 @@ namespace RainMeadow
 
             On.ScavengerAbstractAI.InitGearUp += ScavengerAbstractAI_InitGearUp;
 
+            On.EggBug.DropEggs += EggBug_DropEggs;
+            On.Vulture.DropMask += Vulture_DropMask;
+            On.BigSpider.BabyPuff += BigSpider_BabyPuff;
+
             On.EggBugGraphics.Update += EggBugGraphics_Update;
         }
 
@@ -97,6 +101,37 @@ namespace RainMeadow
         private void OverseerAI_UpdateTempHoverPosition(On.OverseerAI.orig_UpdateTempHoverPosition orig, OverseerAI self)
         {
             if (!self.overseer.abstractCreature.IsLocal()) return;
+            orig(self);
+        }
+
+        // HACK: doesn't play sounds, we should IL hook to only disable spawning
+        private void EggBug_DropEggs(On.EggBug.orig_DropEggs orig, EggBug self)
+        {
+            if (OnlineManager.lobby != null && OnlinePhysicalObject.map.TryGetValue(self.abstractCreature, out var oe) && !oe.isMine)
+            {
+                self.dropEggs = false;
+                return;
+            }
+            orig(self);
+        }
+
+        private void Vulture_DropMask(On.Vulture.orig_DropMask orig, Vulture self, Vector2 violenceDir)
+        {
+            if (OnlineManager.lobby != null && OnlinePhysicalObject.map.TryGetValue(self.abstractCreature, out var oe) && !oe.isMine)
+            {
+                (self.State as Vulture.VultureState).mask = false;
+                return;
+            }
+            orig(self, violenceDir);
+        }
+
+        private void BigSpider_BabyPuff(On.BigSpider.orig_BabyPuff orig, BigSpider self)
+        {
+            if (OnlineManager.lobby != null && OnlinePhysicalObject.map.TryGetValue(self.abstractCreature, out var oe) && !oe.isMine)
+            {
+                self.spewBabies = true;
+                return;
+            }
             orig(self);
         }
     }
